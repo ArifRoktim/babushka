06f036372fce8474b6fffa57a29fce12a9326aef
Michael Ruvinshteyn <mruvinshteyn@stuy.edu>
2018-05-22 13:08:32 -0400

    added logged in/out template for index.html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash, redirect, url_for, request
+from flask import Flask, render_template, flash, redirect, url_for, request, session 
 import os
 import json
 
@@ -6,7 +6,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    if ("user" not in session):
+        return render_template("index.html", selection = "h1")
+    else:
+        return render_template("index.html", selection = None)
 
 if __name__ == "__main__":
     app.debug = True
